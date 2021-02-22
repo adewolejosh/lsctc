801573dc290e09dad2afdbad0c7769541d74c492
Joshua Adewole <adewole.josh@gmail.com>
2021-02-22 23:50:46 +0100

    Setup Node Config
    - setup the necessary config for Postgres and Node

--- lsctc-js-server/index.js
+++ lsctc-js-server/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const {pool} = require('./config')
+const {Pool} = require('pg')
+const db_config = require('./db/config.js')
 
 const app = express()
+const port = 3000
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+const client = new Pool(db_config)
+client.connect(function(){
+    console.log("Connected!")
+})
+
+
+app.listen(port, function(){
+    console.log(`You're listening on port! ${port}`);
+})
